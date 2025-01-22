6bc6123cb0b33add4769e0ab447c7a834616b809
Sherif Fouad <sherifdevv@gmail.com>
2025-01-22 18:19:07 +0200

    dashboard home done

--- src/app/ui/sidebar/sidebar.component.ts
+++ src/app/ui/sidebar/sidebar.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
+  imports: [RouterLink],
 })
 export class SidebarComponent implements OnInit {
   ngOnInit(): void {}
